227196254649cd86f1b510e3a14b2b4fd7fdbb1e
Alexandr Yudenich <devshots.blog@gmail.com>
2021-08-25 22:54:23 +0300

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from 'components/ErrorBoundary';
 
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
 );
