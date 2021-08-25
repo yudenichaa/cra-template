9b05ca0e14527999aa68d0b39a43b9f20a3a8871
Alexandr Yudenich <devshots.blog@gmail.com>
2021-08-25 23:29:14 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
+import { Switch, Route } from 'react-router-dom';
+import { Home } from 'pages';
+
 import './App.scss';
 
 function App() {
   return (
-    <div>
-      <button>Hello world</button>
-      <ul>
-        <li>hello</li>
-        <li>hello</li>
-        <li>hello</li>
-      </ul>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias eum
-        exercitationem at maiores a voluptate ipsam, debitis libero repellendus
-        quis natus quos commodi voluptas ab, magnam temporibus. Sapiente,
-        ratione temporibus.
-      </p>
-    </div>
+    <Switch>
+      <Route path="/">
+        <Home />
+      </Route>
+    </Switch>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ErrorBoundary from 'components/ErrorBoundary';
+import { ErrorBoundary } from 'components';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import './index.scss';
 
 ReactDOM.render(
   <React.StrictMode>
     <ErrorBoundary>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </ErrorBoundary>
   </React.StrictMode>,
   document.getElementById('root')
